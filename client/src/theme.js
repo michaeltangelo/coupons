22a41cb589bec6e41714135b234c6e27383bc655
michaeltangelo <tanman24689@gmail.com>
2018-11-12 15:03:39 -0500

    Changing title and updating signin theme to dark

--- client/src/theme.js
+++ client/src/theme.js
@@ -3,7 +3,7 @@ import blue from '@material-ui/core/colors/blue';
 
 export default createMuiTheme({
   palette: {
-      // type: 'dark',
+      type: 'dark',
       primary: blue,
       // primary: { main: '#1565c0' }, // Purple and green play nicely together.
       // secondary: { main: '#f48fb1' }, // This is just green.A700 as hex.
